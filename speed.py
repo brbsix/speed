17b00500a97e8783d475e2f3312f1539b8007fb8
Six <brbsix@gmail.com>
2016-04-07 12:13:34 -0700

    A pure-Python approach (don't use awk or curl)

--- speed.py
+++ speed.py
@@ -4,16 +4,17 @@
 
 from __future__ import print_function  # Python 2
 
+import errno
 import logging
 import os
 import re
 import subprocess
 import sys
+import urllib3
+
+import requests
 
 __program__ = os.path.basename(os.path.realpath(sys.argv[0]))
-
-# external command dependencies
-DEPENDS = ['curl', 'iperf3']
 
 
 class Speed(object):
@@ -22,102 +23,59 @@ class Speed(object):
     def __init__(self):
         logging.basicConfig(format='%(levelname)s: %(message)s')
 
-        # ensure dependencies are available
-        for dependency in DEPENDS:
-            _ensure(dependency)
-
     @staticmethod
-    def _command(reverse=False):
-        """Craft an appropriate command."""
-        return 'iperf3 -p %s -c %s ' + ('-R ' if reverse else '') + \
-            "| awk '/ sender$/ {print $7, $8}'"
+    def _iperf(port, ip_address, reverse=False):
+        """Run iperf3 with the appropriate arguments then parse the output."""
+        command = ['iperf3', '-p', port, '-c', ip_address] + \
+            (['-R'] if reverse else [])
+
+        with open(os.devnull, 'w') as devnull:  # Python 2
+            try:
+                # run the command and store the output
+                output = subprocess.check_output(
+                    command,
+                    stderr=devnull,
+                    universal_newlines=True)
+            # Py33 raises FileNotFoundError which subclasses OSError
+            # These are not equivalent unless we check the errno attribute
+            except OSError as exc:  # Platform-specific: Python 3.3 and beyond
+                if exc.errno == errno.ENOENT:
+                    logging.error('iperf3 is not installed')
+                    sys.exit(1)
+                raise
+
+        # extract the speed from iperf output
+        for line in output.splitlines():
+            if line.endswith(' sender'):
+                return ' '.join(line.split()[6:8])
 
     @staticmethod
     def _server():
         """Request access to public server, returning port and IP address."""
-        output = _system(
-            ['curl', '--insecure', '-H',
-             'X-Auth-Key: abc', '-H',
-             'X-Auth-Secret: abc', 'https://104.131.128.139/tcp']
-        )
+        url = 'https://104.131.128.139/tcp'
+        headers = {'X-Auth-Key': 'abc', 'X-Auth-Secret': 'abc'}
 
-        data = re.match(
-            r'{"port":(?P<port>[0-9]+),'
-            r'"ip_address":"(?P<ip_address>.*)",'
-            r'"scale":true,"protocol":"tcp"}', output).groupdict()
+        # disable urllib3's InsecureRequestWarning
+        urllib3.disable_warnings(
+            requests.packages.urllib3.exceptions.InsecureRequestWarning)
+        output = requests.get(url, headers=headers, verify=False).text
+
+        pattern = r'{"port":(?P<port>[0-9]+),"ip_address":"' \
+                  r'(?P<ip_address>.*)","scale":true,"protocol":"tcp"}'
+
+        data = re.match(pattern, output).groupdict()
 
         return data['port'], data['ip_address']
 
     @property
     def download(self):
         """Determine download speed."""
-        return _system(self._command(True) % self._server(), shell=True)
+        return self._iperf(*self._server(), reverse=True)
 
     @property
     def upload(self):
         """Determine upload speed."""
-        return _system(self._command() % self._server(), shell=True)
-
-
-def _ensure(executable, warn=False):
-    """Ensure `executable` is installed."""
-
-    try:
-        # Python 3.3+ only
-        from shutil import which
-    except ImportError:
-
-        def which(cmd):
-            """
-            Given a command, return the path which conforms to the given mode
-            on the PATH, or None if there is no such file.
-            """
-
-            def _access_check(path):
-                """
-                Check that a given file can be accessed with the correct mode.
-                Additionally check that `path` is not a directory.
-                """
-                return (os.path.exists(path) and
-                        os.access(path, os.F_OK | os.X_OK) and
-                        not os.path.isdir(path))
-
-            # If we're given a path with a directory part, look it up directly
-            # rather than referring to PATH directories. This includes checking
-            # relative to the current directory, e.g. ./script
-            if os.path.dirname(cmd):
-                if _access_check(cmd):
-                    return cmd
-                return None
-
-            paths = os.environ.get('PATH', os.defpath.lstrip(':')).split(':')
-
-            seen = set()
-            for path in paths:
-                if path not in seen:
-                    seen.add(path)
-                    name = os.path.join(path, cmd)
-                    if _access_check(name):
-                        return name
-            return None
-
-    if which(executable) is None:
-        msg = ('%s is not installed', executable)
-        if warn:
-            logging.warning(*msg)
-        else:
-            logging.fatal(*msg)
-            sys.exit(1)
-
-
-def _system(command, shell=False):
-    """Return the output of a command."""
-    with open(os.devnull, 'w') as devnull:  # Python 2
-        return subprocess.check_output(
-            command,
-            shell=shell,
-            stderr=devnull,
-            universal_newlines=True).strip()
+        return self._iperf(*self._server())
 
 
 def main():
