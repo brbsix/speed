8a22b784a57f2bf576e9296886328fa274f37af5
Six <brbsix@gmail.com>
2016-04-07 08:24:08 -0700

    Ensure external command dependencies are available

--- speed.py
+++ speed.py
@@ -4,15 +4,27 @@
 
 from __future__ import print_function  # Python 2
 
-# standard imports
+import logging
 import os
 import re
 import subprocess
+import sys
 
+__program__ = os.path.basename(os.path.realpath(sys.argv[0]))
+
+# external command dependencies
+DEPENDS = ['curl', 'iperf3']
 
 
 class Speed(object):
     """Test bandwidth speed with iperf3."""
+
+    def __init__(self):
+        logging.basicConfig(format='%(levelname)s: %(message)s')
+
+        # ensure dependencies are available
+        for dependency in DEPENDS:
+            _ensure(dependency)
 
     @staticmethod
     def _command(reverse=False):
@@ -45,6 +57,57 @@ class Speed(object):
     def upload(self):
         """Determine upload speed."""
         return _system(self._command() % self._server(), shell=True)
+
+
+def _ensure(executable, warn=False):
+    """Ensure `executable` is installed."""
+
+    try:
+        # Python 3.3+ only
+        from shutil import which
+    except ImportError:
+
+        def which(cmd):
+            """
+            Given a command, return the path which conforms to the given mode
+            on the PATH, or None if there is no such file.
+            """
+
+            def _access_check(path):
+                """
+                Check that a given file can be accessed with the correct mode.
+                Additionally check that `path` is not a directory.
+                """
+                return (os.path.exists(path) and
+                        os.access(path, os.F_OK | os.X_OK) and
+                        not os.path.isdir(path))
+
+            # If we're given a path with a directory part, look it up directly
+            # rather than referring to PATH directories. This includes checking
+            # relative to the current directory, e.g. ./script
+            if os.path.dirname(cmd):
+                if _access_check(cmd):
+                    return cmd
+                return None
+
+            paths = os.environ.get('PATH', os.defpath.lstrip(':')).split(':')
+
+            seen = set()
+            for path in paths:
+                if path not in seen:
+                    seen.add(path)
+                    name = os.path.join(path, cmd)
+                    if _access_check(name):
+                        return name
+            return None
+
+    if which(executable) is None:
+        msg = ('%s is not installed', executable)
+        if warn:
+            logging.warning(*msg)
+        else:
+            logging.fatal(*msg)
+            sys.exit(1)
 
 
 def _system(command, shell=False):
