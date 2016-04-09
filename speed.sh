5f1ac0207cbff5ae8ab9513956316f0884a158ff
Six <brbsix@gmail.com>
2016-04-08 22:32:28 -0700

    Refactor speed.sh into one function

--- speed.sh
+++ speed.sh
@@ -2,27 +2,10 @@
 #
 # Test bandwidth via public iperf3 server
 
-# run iperf3 and print the parsed output
+# run iperf3 and print the summarized results
 iperf(){
-    local fields ip_address output port reverse
-
-    # use -R flag for download
-    [[ $1 = --reverse ]] && reverse='-R'
-
-    # extract server info from YAML response
-    output=$(request)
-    fields=$(echo "$output" | sed 's/}$//;s/,/\n/g' | sed 's/^.*://;s/"//g')
-    port=$(echo "$fields" | sed -n 1p)
-    ip_address=$(echo "$fields" | sed -n 2p)
-
-    # run iperf and only print the summarized result
-    iperf3 $reverse -p "$port" -c "$ip_address" | awk '/ sender$/ {print $7, $8}'
+    iperf3 $1 $(curl -s --insecure -H 'X-Auth-Key: abc' -H 'X-Auth-Secret: abc' https://104.131.128.139/tcp | awk -F '[:,]' '{gsub("\042", ""); printf "-p %s -c %s", $2, $4}') | awk '/ sender$/ {print $7, $8}'
 }
 
-# use credentials to request access from server
-request(){
-    curl -s --insecure -H 'X-Auth-Key: abc' -H 'X-Auth-Secret: abc' https://104.131.128.139/tcp
-}
-
-echo "Download: $(iperf --reverse)"
+echo "Download: $(iperf -R)"
 echo "Upload: $(iperf)"
