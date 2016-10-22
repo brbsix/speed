26c343b7a2f15b06a4ebf7c75d2e497674b0f03b
Six <brbsix@gmail.com>
2016-10-21 20:10:18 -0700

    Adjust awk script

--- speed.sh
+++ speed.sh
@@ -5,7 +5,7 @@
 # run iperf3 and print the summarized results
 iperf(){
     read -r port address < <(curl -sS --insecure -H 'X-Auth-Key: abc' -H 'X-Auth-Secret: abc' https://104.131.128.139/tcp | awk -F '[:,]' '{gsub("\042", ""); print $2, $4}')
-    iperf3 "$@" -p "$port" -c "$address" | awk '/ sender$/ {print $7, $8}'
+    iperf3 "$@" -p "$port" -c "$address" | awk '$NF == "sender" {print $7, $8; exit}'
 }
 
 set -euo pipefail
